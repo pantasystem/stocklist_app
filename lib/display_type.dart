6f71d8376e2ef4a62b203d42c8218ee991a14d71
kinoshita <kinoshita062323@gmail.com>
2021-04-22 22:43:35 +0900

    表示状態のタイプ

---
+++ lib/display_type.dart
@@ -0,0 +1,19 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// 各要素（Item, Node）などの表示タイプ
+enum DisplayType {
+  LIST, GRID
+}
+
+class DisplayTypeState extends StateNotifier<DisplayType> {
+  DisplayTypeState(DisplayType type) : super(type);
+
+  void changeToGrid() {
+    state = DisplayType.GRID;
+  }
+
+  void changeToList() {
+    state = DisplayType.LIST;
+  }
+
+}
