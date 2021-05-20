e576dfcd38757fffeb06ae1cc94eb6639076de97
kinoshita0623 <kuutarousii@gmail.com>
2021-05-20 17:36:13 +0900

    収納を表示するCardを実装した

--- lib/widget/stock_widget.dart
+++ lib/widget/stock_widget.dart
@@ -22,21 +22,32 @@ class StockCardWidget extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
-    final box = useProvider(boxesStateProvider.notifier).get(stock.boxId!)!;
+    final box = useProvider(boxesStateProvider.notifier).get(stock.boxId)!;
     return Card(
       child: Container(
         child: Column(
           children: [
-            Container(
-              child: Text(box.name),
-            ),
             Row(
               children: [
-                IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-                IconButton(onPressed: (){}, icon: Icon(Icons.repeat_one_on_outlined))
+                Container(
+                  child: Text(box.name),
+                  margin: EdgeInsets.all(16),
+                ),
+
+                IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: (){
+
+                  }
+                )
               ],
-              mainAxisAlignment: MainAxisAlignment.end,
-            )
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+
+            StockCountWidget(count: stock.count, countChangedListener: (int count){
+
+            }),
+
           ],
 
         ),
@@ -44,6 +55,32 @@ class StockCardWidget extends HookWidget {
       )
 
 
+    );
+  }
+}
+
+typedef CountChangedListener = void Function(int count);
+
+class StockCountWidget extends StatelessWidget {
+
+  final int count;
+  final CountChangedListener countChangedListener;
+
+  StockCountWidget({ required this.count, required this.countChangedListener });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if(count > 0)
+          IconButton(onPressed: (){
+            countChangedListener(count + 1);
+          }, icon: Icon(Icons.remove)),
+        Text(count.toString()),
+        IconButton(onPressed: (){}, icon: Icon(Icons.add)),
+
+      ],
+      mainAxisAlignment: MainAxisAlignment.end,
     );
   }
 }
