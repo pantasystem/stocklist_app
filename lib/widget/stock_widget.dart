cc157184bc8f3e709ff6bd9b3a9172906b259644
kinoshita <kinoshita062323@gmail.com>
2021-05-23 02:21:03 +0900

    消費期限を表示するようにした

--- lib/widget/stock_widget.dart
+++ lib/widget/stock_widget.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:stocklist_app/entity/box.dart';
 import 'package:stocklist_app/entity/item.dart';
 import 'package:stocklist_app/entity/stock.dart';
@@ -83,10 +84,31 @@ class StockCardWidget extends HookWidget {
                 ],
               )
             ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if(stock.expirationDate != null)
+                  Container(
+                    padding: EdgeInsets.only(left: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('消費期限:'),
+                        SizedBox(width: 2),
+                        Text(DateFormat.yMd(Intl.defaultLocale).format(stock.expirationDate!))
+                      ],
+                    ),
+                  ),
+                if(stock.expirationDate == null)
+                  Container(),
+                StockCountWidget(count: stock.count, countChangedListener: (int count){
 
-            StockCountWidget(count: stock.count, countChangedListener: (int count){
+                }),
 
-            }),
+              ],
+            ),
+
+
 
           ],
 
