837007e829585197dda948264b0e4dab39c13873
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-07-15 15:47:30 +0900

    Storeの一部を実装しました

--- lib/store/shopping_list_store.dart
+++ lib/store/shopping_list_store.dart
@@ -2,29 +2,53 @@
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:stocklist_app/entity/shopping_list.dart';
+import 'package:stocklist_app/main.dart';
 import 'package:stocklist_app/state/shopping_list_state.dart';
 
 class ShoppingListStore extends StateNotifier<ShoppingListsState> {
-  ShoppingListStore() : super(ShoppingListsState(shoppingLists: const []));
+  ShoppingListStore(this.read) : super(ShoppingListsState(shoppingLists: const []));
+
+  Reader read;
+
 
   void replaceAll(List<ShoppingList> lists) {
     this.state = this.state.copyWith(shoppingLists: lists);
   }
 
   void add(ShoppingList sl) {
-
+    final index = this.state.shoppingLists.indexWhere((element) => element.id == sl.id);
+    if(index == -1) {
+      this.state = this.state.copyWith(
+        shoppingLists: [
+          ...this.state.shoppingLists,
+          sl
+        ]
+      );
+    }else{
+      final l = [
+        ...this.state.shoppingLists,
+      ];
+      l.add(sl);
+      this.state = this.state.copyWith(
+          shoppingLists:l
+      );
+    }
   }
 
   void addAll(List<ShoppingList> lists) {
-
+    lists.forEach((element) {
+      add(element);
+    });
   }
 
   Future fetchAll() async{
-
+    final list = await stocklistClient.shoppingListAPI.all();
+    read(storeAdder).addShoppingListDTOs(list);
   }
 
   Future fetch(int id) async{
-
+    final sl = await stocklistClient.shoppingListAPI.show(id);
+    read(storeAdder).addShoppingListDTO(sl);
   }
 
   Future create() async {
