7633ad998deb38763fee3472aaa07a68f59d3f7f
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-07-26 13:35:53 +0900

    Task作成用のオブジェクトを作成しました

---
+++ lib/entity/create_shopping_task.dart
@@ -0,0 +1,10 @@
+
+
+class CreateShoppingTask {
+  final int listId;
+  final int count;
+  final int boxId;
+  final int itemId;
+
+  CreateShoppingTask({required this.listId, required this.count, required this.boxId, required this.itemId});
+}
