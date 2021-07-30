9a8140ec542786166ccb4975c7329005ed99b782
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-07-30 16:15:24 +0900

    コンポーネントとして切り出し

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -49,46 +49,81 @@ class HomeScreen extends HookWidget {
       body: ListView(
         padding: EdgeInsets.all(8),
         children: [
-          Card(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  child: Text(
-                    '消費期限が近い物',
-                    style: TextStyle(
-                        fontSize: 20
-                    ),
-                  ),
-                  padding: EdgeInsets.all(16),
-                ),
-                Divider(
-                  height: 2,
-                ),
-                ItemListView(
-                  items: a,
-                  shrinkWrap: true,
-                  physics: ClampingScrollPhysics(),
-                  onItemSelected: (int index, Item item) {
-                    Navigator.of(context).pushNamed('/items/show', arguments: ItemArgs(item.id));
-                  },
-                ),
-                Divider(
-                  height: 2,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: IconButton(
-                    icon: itemsAllShow.value ? Icon(Icons.expand_less) : Icon(Icons.expand_more_outlined), onPressed: (){
-                      itemsAllShow.value = !itemsAllShow.value;
-                  })
-                )
+          NearExpirationDateItemsCard(),
+          SizedBox(height: 8),
 
-              ],
-            )
-          )
+
         ],
       ),
+    );
+  }
+}
+
+class NearExpirationDateItemsCard extends HookWidget {
+  @override
+  Widget build(BuildContext context) {
+    final itemStore = useProvider(itemsStateProvider.notifier);
+    final List<Item> items = useProvider(itemsStateProvider).items;
+    final itemsAllShow = useState(false);
+
+    var a = items.sorted((a, b) {
+      final aMax = a.stockExpiries.maxOrNull;
+      final bMax = b.stockExpiries.maxOrNull;
+      if(aMax == null && bMax == null) {
+        return 0;
+      }
+      if(aMax == null) {
+        return 1;
+      }
+      if(bMax == null) {
+        return -1;
+      }
+      return aMax.compareTo(bMax);
+    }).toList();
+    if(!itemsAllShow.value && a.length > 3) {
+      a = a.sublist(0, 3);
+    }
+    if(a.length > 10) {
+      a = a.sublist(0, 10);
+    }
+
+    return Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Text(
+                '消費期限が近い物',
+                style: TextStyle(
+                    fontSize: 20
+                ),
+              ),
+              padding: EdgeInsets.all(16),
+            ),
+            Divider(
+              height: 2,
+            ),
+            ItemListView(
+              items: a,
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              onItemSelected: (int index, Item item) {
+                Navigator.of(context).pushNamed('/items/show', arguments: ItemArgs(item.id));
+              },
+            ),
+            Divider(
+              height: 2,
+            ),
+            Container(
+                alignment: Alignment.center,
+                child: IconButton(
+                    icon: itemsAllShow.value ? Icon(Icons.expand_less) : Icon(Icons.expand_more_outlined), onPressed: (){
+                  itemsAllShow.value = !itemsAllShow.value;
+                })
+            )
+
+          ],
+        )
     );
   }
 }
