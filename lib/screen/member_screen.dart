f9f30a6ae415bae8a6e80d54857a50e7a4dda053
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-08-30 12:41:57 +0900

    メンバーを一覧表示できるようにしました

--- lib/screen/member_screen.dart
+++ lib/screen/member_screen.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:stocklist_app/entity/user.dart';
 import 'package:stocklist_app/main.dart';
 import 'package:stocklist_app/screen/shopping_list_detail_screen.dart';
 
@@ -9,6 +11,11 @@ class MemberScreen extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final homeStore = useProvider(homeStoreProvider.notifier);
+    final List<User> members = useProvider(homeStoreProvider).home?.members ?? [];
+    useEffect((){
+      Future.microtask(()=>homeStore.fetch());
+    }, []);
     return Scaffold(
       appBar: AppBar(title: Text('メンバー一覧')),
       floatingActionButton: FloatingActionButton.extended(
@@ -18,6 +25,16 @@ class MemberScreen extends HookWidget {
         label: Text('招待'),
         icon: Icon(Icons.add)
       ),
+      body: () {
+        if(members.isEmpty) {
+          return Container(child: CircleProgress());
+        }
+        return ListView.builder(
+          itemCount: members.length,
+          itemBuilder: (context, pos) {
+            return ListTile(title: Text(members[pos].name));
+          });
+      }.call()
     );
   }
 }
